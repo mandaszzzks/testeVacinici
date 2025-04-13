6839fb54fa501726d008fba0dae7c9f7808dc7d4
mandaszzzks <3mandasouza2018@gmail.com>
2025-04-13 12:26:25 -0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-container">
+        <div>
+          <h3>Serviços</h3>
+          <ul>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Termos e Condições </a></li>
+            <li><a href="#">Política de Privacidade</a></li>
+          </ul>
+        </div>
+        <div>
+          <h3>Sobre a gente</h3>
+          <ul>
+            <li><a href="#">Our story</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+        </div>
+        <div>
+          <h3>Acompanhe a gente </h3>
+          <div className="footer-icons">
+          <img src="src\assets\imagem_2025-03-30_154947541-removebg-preview.png" alt="Descrição da kakakak" />
+          <img src="src\assets\facebook.png" alt="Descrição da kakakak" />
+          <img src="src\assets\twitter.png" alt="Descrição da kakakak" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -30,10 +30,17 @@ const Home = () => {
         </div>
 
   
+       
 
         {/* Seção de Testemunhos */}
         <section className="testimonials py-5">
-          <h2 className="text-center mb-5">O que dizem nossos usuários?</h2>
+          <div className="saiba">
+            <h3>Saiba mais</h3>
+          </div>
+
+          <div className="saiba2">
+          <p> Sobre nosso aplicativo e vacinas:        </p>
+          </div>
 
           <div className="testimonials-grid">
             {/* Testemunho 1 */}
@@ -48,15 +55,7 @@ const Home = () => {
               </div>
             </div>
 
-    {/* Imagem Ilustrativa 
-    <div className="col-12 text-center mt-4">
-          <img
-            src={GraficoImg}
-            alt="Imagem ilustrativa"
-            className="grafico"
-          />
-        </div>
-        */}
+        
         
             {/* Testemunho 2 */}
             <div className="card">
@@ -96,12 +95,31 @@ const Home = () => {
           </div>
         </section>
 
+
+        <div className="grafico">
+   <img src="src\assets\grafico.png" alt="Descrição da kakakak" />
+   </div>
+
+
+             
+        <div className="col-12 frase1">
+  <h2>Para que serve a vacinação?</h2>
+  <p className="subtitulo">
+      Redução de Doenças: A vacinação ajudou a reduzir a incidência de doenças como sarampo, poliomielite
+      , difteria e tétano em mais de 90% globalmente.<p></p>
+      Impacto Econômico: A cada US$ 1 investido em programas de vacinação, estima-se que há um retorno de US$ 44 em benefícios de saúde.<p></p>
+      Erradicação de Doenças: A varíola foi erradicada em 1980 graças a campanhas de vacinação em massa.
+      <p></p>
+      Prevenção de Mortes: Estima-se que vacinas prevenem cerca de 2 a 3 milhões de mortes por ano, de acordo com a Organização Mundial da Saúde (OMS).
+        </p>
+    </div>
+
         {/* Seção de Vídeo */}
         <div className="col-12 text-center mt-4">
-          <div className="embed-responsive embed-responsive-16by9">
-            <video className="embed-responsive-item" autoPlay loop muted>
-              <source src={VideoSrc} type="video/mp4" />
-              Seu navegador não suporta vídeos.
+  <div className="video-container">
+    <video className="video" autoPlay loop muted>
+      <source src={VideoSrc} type="video/mp4" />
+      Seu navegador não suporta vídeos.
             </video>
           </div>
         </div>
