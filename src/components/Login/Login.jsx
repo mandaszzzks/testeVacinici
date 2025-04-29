d94b933316223567b6dd8d16fd10c63854af3f5d
mandaszzzks <3mandasouza2018@gmail.com>
2025-04-29 20:13:43 -0300

    Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,34 +1,34 @@
+import './Login.css'; // <- Isso é essencial!
+
+
+
 export default function Login() {
-    return (
+  
+  return (
+    <div className="login-page vh-100 d-flex align-items-center justify-content-center">
+      <div className="row w-75 shadow-lg rounded p-5 login-box">
         
-      <div className="container-fluid vh-100 d-flex align-items-center justify-content-center bg-light">
-        <div className="row w-75 shadow-lg rounded p-5 bg-white login-box">
+        {/* Seção de Login */}
+        <div className="col-md-6 d-flex flex-column login-title">
+          <h2 className="fw-bold">Seja bem-vindo de volta!</h2>
+          <p className="frase4"><strong>Preencha seus dados pessoais.</strong></p>
           
-  
-          {/* Seção de Login */}
-          <div className="col-md-6 d-flex flex-column justify-content-center">
-            <h2 className="fw-bold">Seja bem-vindo de volta!</h2>
-            <p className="fr"><strong>Preencha seus dados pessoais.</strong></p>
-            
-            <form>
-              <div className="mb-3">
-                <input type="text" className="form-control" placeholder="Username" />
-              </div>
-              <div className="mb-3 position-relative">
-                <input type="password" className="form-control" placeholder="Password" />
-              </div>
-              {/* Botão de Entrar */}
-              <button className="btn btn-dark w-100">Entrar</button>
-            </form>
-          </div>
-          
-          {/* Seção de Ilustração */}
-          <div className="col-md-6 bg-light d-flex flex-column align-items-center justify-content-center rounded">
-            <img src="src/assets/vaciniilus.png" alt="Illustration" className="img-fluid" />
-          </div>
+          <form>
+            <div className="mb-3 form-input">
+              <input type="text" className="form-control" placeholder="Username" />
+            </div>
+            <div className="mb-3 position-relative form-input">
+              <input type="password" className="form-control" placeholder="Password" />
+            </div>
+            <button className="btn btn-dark w-100 btn-login">Entrar</button>
+          </form>
         </div>
-      
+        
+        {/* Seção de Ilustração */}
+        <div className="col-md-6 bg-light d-flex flex-column align-items-center justify-content-center rounded login-right">
+          <img src="src/assets/vaciniilus.png" alt="Illustration" className="img-fluid" />
+        </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
